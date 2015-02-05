00aeb09d15628f3daf11cb559ee76ca957919e4c
jr829 <jr829@york.ac.uk>
2015-02-05 16:54:10 +0000

    Added several get set functions and arrayList
    Signed-off-by: jr829 <jr829@york.ac.uk>

--- TeachEasy/src/teacheasy/data/TextObject.java
+++ TeachEasy/src/teacheasy/data/TextObject.java
@@ -1,5 +1,50 @@
 package teacheasy.data;
 
-public class TextObject {
+import java.util.ArrayList;
 
+/**
+ @version 	1.0 05 Feb 2015
+ @author 	Daniel Berhe & Jake Ransom*/
+
+
+public class TextObject extends PageObject {
+	private int fontSize;
+	private String fontColour;
+	private String textFont;
+	ArrayList <String> list; 
+	
+	public TextObject(float nXStart, float nYStart) {
+		super(PageObjectType.TEXT, nXStart, nYStart);
+		
+	}
+
+	public int getFontSize() {
+		return fontSize;
+	}
+
+	public void setFontSize(int newfontSize) {
+		this.fontSize = newfontSize;
+	}
+
+	public String getFontColour() {
+		return fontColour;
+	}
+
+	public void setFontColour(String newfontColour) {
+		this.fontColour = newfontColour;
+	}
+
+	public String getTextFont() {
+		return textFont;
+	}
+
+	public void setTextFont(String newtextFont) {
+		this.textFont = newtextFont;
+	}
+	
+	
+	private void storeString(){
+		list = new ArrayList <String> ();
+	}
+	
 }
