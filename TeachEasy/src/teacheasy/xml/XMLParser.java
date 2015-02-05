779f12849adc256abf3addd9e1c2d279ffb29503
amj523 <amj523@york.ac.uk>
2015-02-05 12:50:38 +0000

    Created skeleton for the XMLParser class
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/xml/XMLParser.java
+++ TeachEasy/src/teacheasy/xml/XMLParser.java
@@ -1,5 +1,34 @@
+/*
+ * Alistair Jewers
+ * 
+ * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
+ * 
+ */
 package teacheasy.xml;
 
-public class XMLParser {
+import teacheasy.data.Lesson;
+import teacheasy.data.Page;
+import teacheasy.data.PageObject;
 
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+
+/**
+ * This class handles XML parsing, 
+ * storing the data as Lesson objects.
+ * 
+ * @version 	1.0 05 Feb 2015
+ * @author 		Alistair Jewers
+ */
+public class XMLParser extends DefaultHandler{
+	/** Lesson being constructed */
+	Lesson currentLesson;
+	
+	/** Constructor Method */
+	public XMLParser() {
+		
+	}
 }
