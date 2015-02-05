9e99a0c1ba9c862b27fcd68d645614b68ae6a758
sr896 <sr896@ELCLASS21.its.york.ac.uk>
2015-02-05 17:01:46 +0000

    Adjusted to better fit style
    Signed-off-by: sr896 <sr896@ELCLASS21.its.york.ac.uk> and sam hall

--- TeachEasy/src/teacheasy/data/AnswerBoxData.java
+++ TeachEasy/src/teacheasy/data/AnswerBoxData.java
@@ -4,47 +4,40 @@
  * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
  */
 package teacheasy.data;
+
 /**
  * Class for holding answer box data
  * @author sr896 and sh1157
  *
  */
 public class AnswerBoxData extends PageObject {
-	
 	int characterLimit, marks;
 	String correctAnswers;
 	boolean retry;
-	
 	
 	public int getCharacterLimit() {
 		return characterLimit;
 	}
 
-
 	public void setCharacterLimit(int characterLimit) {
 		this.characterLimit = characterLimit;
 	}
-
 
 	public int getMarks() {
 		return marks;
 	}
 
-
 	public void setMarks(int marks) {
 		this.marks = marks;
 	}
-
 
 	public String getCorrectAnswers() {
 		return correctAnswers;
 	}
 
-
 	public void setCorrectAnswers(String correctAnswers) {
 		this.correctAnswers = correctAnswers;
 	}
-
 
 	public boolean isRetry() {
 		return retry;
