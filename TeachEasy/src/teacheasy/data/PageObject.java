510d17733821293e379595cbded01afa8a69de81
amj523 <amj523@york.ac.uk>
2015-02-05 11:01:18 +0000

    Created skeleton of PageObject class. Included an enumeration of the page object types and xstart/ystart values as these are common to all page objects.
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/data/PageObject.java
+++ TeachEasy/src/teacheasy/data/PageObject.java
@@ -1,5 +1,49 @@
+/*
+ * Alistair Jewers
+ * 
+ * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
+ * 
+ */
 package teacheasy.data;
 
+/**
+ * This class encapsulates a single object 
+ * on a page.
+ * 
+ * @version 	1.0 05 Feb 2015
+ * @author 		Alistair Jewers
+ */
 public abstract class PageObject {
-
+	
+	/** Enumeration of the various page object types */
+	public static enum PageObjectType {
+		TEXT,
+		PICTURE,
+		GRAPHIC,
+		AUDIO,
+		VIDEO,
+		ANSWER_BOX,
+		MULTIPLE_CHOICE,
+		BUTTON
+	}
+	
+	/** The type of this page object */
+	private PageObjectType type;
+	
+	/** Relative X and Y position, measured from top left */
+	private float xStart;
+	private float yStart;
+	
+	/**
+	 * Constructor Method
+	 * 
+	 * @param nType - The type of this page object
+	 * @param nXStart - Relative X position
+	 * @param nYStart - Relative Y position
+	 */
+	public PageObject(PageObjectType nType, float nXStart, float nYStart) {
+		this.type = nType;
+		this.xStart = nXStart;
+		this.yStart = nYStart;
+	}
 }
