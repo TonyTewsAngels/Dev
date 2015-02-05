802dcb7938d99b05c5f0b1b780d3c4b2c1f3fd26
amj523 <amj523@york.ac.uk>
2015-02-05 11:15:23 +0000

    Created skeleton of Iteration1DummyUI class
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/debug/Iteration1DummyUI.java
+++ TeachEasy/src/teacheasy/debug/Iteration1DummyUI.java
@@ -1,13 +1,35 @@
+/*
+ * Alistair Jewers
+ * 
+ * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
+ * 
+ */
 package teacheasy.debug;
 
+import teacheasy.xml.XMLHandler;
+
+/**
+ * This class provides a dummy UI for
+ * use with Iteration 1 code.
+ * 
+ * @version 	1.0 05 Feb 2015
+ * @author 		Alistair Jewers
+ */
 public class Iteration1DummyUI {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/** Handles XML read and write operations */
+	private XMLHandler xmlHandler;
+	
+	/** Constructor Method */
+	public Iteration1DummyUI() {
+		
+		/* Instantiate XML Handler */
+		xmlHandler = new XMLHandler();
+		
+		// Create GUI Frame
 	}
 
+	/** Main Function called when application is run */
+	public static void main(String[] args) {
+		new Iteration1DummyUI();
+	}
 }
