b523609a69873d768e6f770bc4d546ad2b7dd38a
amj523 <amj523@york.ac.uk>
2015-02-05 11:30:57 +0000

    Created the skeleton of the XML Handler
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/xml/XMLHandler.java
+++ TeachEasy/src/teacheasy/xml/XMLHandler.java
@@ -1,5 +1,34 @@
+/*
+ * Alistair Jewers
+ * 
+ * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
+ * 
+ */
 package teacheasy.xml;
 
+/**
+ * This class handles XML reading and
+ * writing whilst maintaining info
+ * about file locations (recent files).
+ * 
+ * @version 	1.0 05 Feb 2015
+ * @author 		Alistair Jewers
+ */
 public class XMLHandler {
-
+	
+	/** XML Parser */
+	private XMLParser xmlParser;
+	
+	/** XML Writer */
+	private XMLWriter xmlWriter;
+	
+	/** Constructor Method */
+	public XMLHandler() {
+		
+		/* Instantiate the Parser */
+		xmlParser = new XMLParser();
+		
+		/* Instantiate the Writer */
+		xmlWriter = new XMLWriter();
+	}
 }
