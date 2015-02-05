c4ce5dfeeee0588dfca2f7e45db36c93599fa6d6
amj523 <amj523@york.ac.uk>
2015-02-05 10:24:01 +0000

    Created skeleton of Lesson class. Added container for pages.
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/data/Lesson.java
+++ TeachEasy/src/teacheasy/data/Lesson.java
@@ -1,5 +1,30 @@
+/*
+ * Alistair Jewers
+ * 
+ * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
+ * 
+ */
 package teacheasy.data;
 
-public class Lesson {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * This class encapsulates a single Lesson
+ * within the TeachEasy product.
+ * 
+ * @version 	1.0 05 Feb 2015
+ * @author 		Alistair Jewers
+ */
+public class Lesson {
+	
+	/** Container for the pages within this lesson */
+	public List<Page> pages;
+	
+	/** Constructor Method */
+	public Lesson() {
+		
+		/* Instantiate the pages container */
+		pages = new ArrayList<Page>();
+	}
 }
