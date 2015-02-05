d2f50f0326bf1408a22216a8e21c5294f96fe5fa
amj523 <amj523@york.ac.uk>
2015-02-05 10:30:22 +0000

    Created the skeleton for the Page class.
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/data/Page.java
+++ TeachEasy/src/teacheasy/data/Page.java
@@ -1,5 +1,30 @@
+/*
+ * Alistair Jewers
+ * 
+ * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
+ * 
+ */
 package teacheasy.data;
 
-public class Page {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * This class encapsulates a single page
+ * within a Lesson.
+ * 
+ * @version 	1.0 05 Feb 2015
+ * @author 		Alistair Jewers
+ */
+public class Page {
+	
+	/** Container for the objects on this page */
+	public List<PageObject> pageObjects;
+	
+	/** Constructor Method */
+	public Page() {
+		
+		/* Instantiate the page object container */
+		pageObjects = new ArrayList<PageObject>();
+	}
 }
