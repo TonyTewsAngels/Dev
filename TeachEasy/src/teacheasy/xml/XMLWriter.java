34c4b1b6b23a837bd392506f12b1732dfd57a663
amj523 <amj523@york.ac.uk>
2015-02-05 12:53:55 +0000

    Created skeleton for XMLWriter class
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/xml/XMLWriter.java
+++ TeachEasy/src/teacheasy/xml/XMLWriter.java
@@ -1,5 +1,26 @@
+/*
+ * Alistair Jewers
+ * 
+ * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
+ * 
+ */
 package teacheasy.xml;
 
-public class XMLWriter {
+import teacheasy.data.Lesson;
+import teacheasy.data.Page;
+import teacheasy.data.PageObject;
 
+/**
+ * This class handles XML writing, 
+ * based on data stored in Lesson objects.
+ * 
+ * @version 	1.0 05 Feb 2015
+ * @author 		Alistair Jewers
+ */
+public class XMLWriter {
+	
+	/** Constructor Method */
+	public XMLWriter() {
+		
+	}
 }
