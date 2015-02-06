9676099dc212d8b1d3ff23200df50fa902e257be
sh1157 <sh1157@ELCLASS20.its.york.ac.uk>
2015-02-06 13:23:52 +0000

    Style Changes
    Signed-off-by: sh1157 <sh1157@ELCLASS20.its.york.ac.uk>

--- TeachEasy/src/teacheasy/data/PictureData.java
+++ TeachEasy/src/teacheasy/data/PictureData.java
@@ -1,3 +1,9 @@
+/*
+ * Sam Hall and Sam Raeburn
+ * 
+ * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
+ * 
+ */
 package teacheasy.data;
 
 import teacheasy.data.PageObject.PageObjectType;
@@ -10,51 +16,39 @@ import teacheasy.data.PageObject.PageObjectType;
 public class PictureData extends PageObject {
 	String url;
 	String uri;
-	float xScaleFactor, yScaleFactor, rotation;
-	
+	float xScaleFactor, yScaleFactor;
+	float rotation;
 	/** Constructor Method */
 	public PictureData (PageObjectType nType, float nXStart, float nYStart) {
 		/* Must call the constructor method of superclass */
 		super(PageObjectType.PICTURE, nXStart, nYStart);	
 	}
-	
 	/** Method to get the URL */
 	public String getUrl() {
 		return url;
 	}
-
 	public void setUrl(String nUrl) {
 		this.url = nUrl;
 	}
-
 	public String getUri() {
 		return uri;
 	}
-
 	public void setUri(String nUri) {
 		this.uri = nUri;
 	}
-
 	public float getyScaleFactor() {
 		return yScaleFactor;
 	}
-
 	public void setyScaleFactor(float yScaleFactor) {
 		this.yScaleFactor = yScaleFactor;
 	}
-
 	public float getRotation() {
 		return rotation;
 	}
-
 	public void setRotation(float nRotation) {
 		this.rotation = nRotation;
 	}
-
 	public float getxScaleFactor() {
 		return xScaleFactor;
 	}
-
-
-	
 }
