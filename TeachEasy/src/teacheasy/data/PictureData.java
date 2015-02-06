9d2e08c59ec36d09a207a66f2ee28eed4bb0bb2b
sh1157 <sh1157@ELCLASS20.its.york.ac.uk>
2015-02-06 13:55:30 +0000

    Yet another style change
    Signed-off-by: sh1157 <sh1157@ELCLASS20.its.york.ac.uk>

--- TeachEasy/src/teacheasy/data/PictureData.java
+++ TeachEasy/src/teacheasy/data/PictureData.java
@@ -18,36 +18,45 @@ public class PictureData extends PageObject {
 	String uri;
 	float xScaleFactor, yScaleFactor;
 	float rotation;
+	
 	/** Constructor Method */
 	public PictureData (PageObjectType nType, float nXStart, float nYStart) {
 		/* Must call the constructor method of superclass */
 		super(PageObjectType.PICTURE, nXStart, nYStart);	
 	}
+	
 	/** Method to get the URL */
 	public String getUrl() {
 		return url;
 	}
+	
 	public void setUrl(String nUrl) {
 		this.url = nUrl;
 	}
+	
 	public String getUri() {
 		return uri;
 	}
+	
 	public void setUri(String nUri) {
 		this.uri = nUri;
 	}
+	
 	public float getyScaleFactor() {
 		return yScaleFactor;
 	}
 	public void setyScaleFactor(float yScaleFactor) {
 		this.yScaleFactor = yScaleFactor;
 	}
+	
 	public float getRotation() {
 		return rotation;
 	}
+	
 	public void setRotation(float nRotation) {
 		this.rotation = nRotation;
 	}
+	
 	public float getxScaleFactor() {
 		return xScaleFactor;
 	}
