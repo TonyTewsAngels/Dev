7783b82d424976cdb19f560621dd13abeeb1bee4
amj523 <amj523@york.ac.uk>
2015-02-08 12:32:50 +0000

    Added methods to manipulate the objects in the page container. #20
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/data/Page.java
+++ TeachEasy/src/teacheasy/data/Page.java
@@ -13,18 +13,51 @@ import java.util.List;
  * This class encapsulates a single page
  * within a Lesson.
  * 
- * @version 	1.0 05 Feb 2015
+ * @version 	1.1 08 Feb 2015
  * @author 		Alistair Jewers
  */
-public class Page {
-	
+public class Page { 
+    
 	/** Container for the objects on this page */
 	public List<PageObject> pageObjects;
 	
 	/** Constructor Method */
 	public Page() {
-		
-		/* Instantiate the page object container */
+	    /* Instantiate the page object container */
 		pageObjects = new ArrayList<PageObject>();
+	}
+	
+	/** Add a new object to the page */
+	public void addObject(PageObject pageObject) {
+	    pageObjects.add(pageObject);
+	}
+	
+	/** Remove an object from the page */
+	public void removeObject(int index) {
+	    pageObjects.remove(index);
+	    //TODO Index error handling?
+	}
+	
+	/** Get an object on the page */
+	public PageObject getObject(int index) {
+	    return pageObjects.get(index);
+	    //TODO Index error handling?    
+	}
+	
+	/** Move an object to another layer */
+	public void moveObject(int oldPosition, int newPosition) {
+	    /* Create temporary copy of the object */
+	    PageObject temp = pageObjects.get(oldPosition);
+	    
+	    /* Remove old object, Add the temporary copy at the new location */
+	    pageObjects.remove(oldPosition);
+	    pageObjects.add(newPosition, temp);
+	    
+	    //TODO Index error handling?
+	}
+	
+	/** Get page object count */
+	public int getObjectCount() {
+	    return pageObjects.size();
 	}
 }
