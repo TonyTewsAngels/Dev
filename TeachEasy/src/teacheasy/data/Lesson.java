61bd7aa8d9288db8a719de63aff67cc8e2a80a43
amj523 <amj523@york.ac.uk>
2015-02-08 15:46:57 +0000

    Added the metadata to the Lesson class. #21
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/data/Lesson.java
+++ TeachEasy/src/teacheasy/data/Lesson.java
@@ -9,11 +9,13 @@ package teacheasy.data;
 import java.util.ArrayList;
 import java.util.List;
 
+import teacheasy.data.lessondata.*;
+
 /**
  * This class encapsulates a single Lesson
  * within the TeachEasy product.
  * 
- * @version 	1.0 05 Feb 2015
+ * @version 	1.1 08 Feb 2015
  * @author 		Alistair Jewers
  */
 public class Lesson {
@@ -21,10 +23,34 @@ public class Lesson {
 	/** Container for the pages within this lesson */
 	public List<Page> pages;
 	
+	/** Metadata Object */
+	public LessonInfo lessonInfo;
+	
+	/** Default Settings Object */
+	public LessonDefaultSettings defaultSettings;
+	
+	/** Grade Settings Object */
+	public LessonGradeSettings gradeSettings;
+	
 	/** Constructor Method */
-	public Lesson() {
+	public Lesson(String author, String version, String comment,
+	              String dateCreated, int totalMarks, String lessonName,
+	              String dfBackgroundColour, String dfFont, int dfFontSize,
+	              String dfFontColour, String dfLineColour, String dfFillColour,
+	              int passMark, String passMessage, String failMessage) {
 		
 		/* Instantiate the pages container */
 		pages = new ArrayList<Page>();
+		
+		/* Instantiate the metadata objects */
+		lessonInfo = new LessonInfo(lessonName, author, version,
+		                            comment, dateCreated, totalMarks);
+		
+		defaultSettings = new LessonDefaultSettings(dfFontSize, dfFont,
+		                            dfBackgroundColour, dfFontColour,
+		                            dfLineColour, dfFillColour);
+		
+		gradeSettings = new LessonGradeSettings(passMark, passMessage,
+		                            failMessage);
 	}
 }
