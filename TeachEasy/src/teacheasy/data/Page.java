472e82e09a183b83df16634893904143efe8a839
amj523 <amj523@york.ac.uk>
2015-02-08 13:20:26 +0000

    Formatting. #20
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/data/Page.java
+++ TeachEasy/src/teacheasy/data/Page.java
@@ -21,58 +21,58 @@ public class Page {
     private String backgroundColour; // 8 digit hex code, RGBA
     
 	/** Container for the objects on this page */
-	public List<PageObject> pageObjects;
-	
+    public List<PageObject> pageObjects;
+    
 	/** Constructor Method */
-	public Page(String nBackgroundColour) {
-	    /* Instantiate class level variables */
-	    this.backgroundColour = nBackgroundColour;
-	    
+    public Page(String nBackgroundColour) {
+        /* Instantiate class level variables */
+        this.backgroundColour = nBackgroundColour;
+        
 	    /* Instantiate the page object container */
-		pageObjects = new ArrayList<PageObject>();
-	}
+        pageObjects = new ArrayList<PageObject>();
+        }
 	
 	/** Add a new object to the page */
-	public void addObject(PageObject pageObject) {
-	    pageObjects.add(pageObject);
+    public void addObject(PageObject pageObject) {
+        pageObjects.add(pageObject);
 	}
-	
+    
 	/** Remove an object from the page */
-	public void removeObject(int index) {
-	    pageObjects.remove(index);
-	    //TODO Index error handling?
-	}
+    public void removeObject(int index) {
+        pageObjects.remove(index);
+        //TODO Index error handling?
+        }
 	
 	/** Get an object on the page */
-	public PageObject getObject(int index) {
-	    return pageObjects.get(index);
-	    //TODO Index error handling?    
-	}
-	
-	/** Move an object to another layer */
-	public void moveObject(int oldPosition, int newPosition) {
-	    /* Create temporary copy of the object */
-	    PageObject temp = pageObjects.get(oldPosition);
-	    
-	    /* Remove old object, Add the temporary copy at the new location */
-	    pageObjects.remove(oldPosition);
-	    pageObjects.add(newPosition, temp);
-	    
-	    //TODO Index error handling?
-	}
-	
-	/** Get page object count */
-	public int getObjectCount() {
-	    return pageObjects.size();
-	}
-	
+    public PageObject getObject(int index) {
+        return pageObjects.get(index);
+        //TODO Index error handling?    
+    }
+    
+    /** Move an object to another layer */
+    public void moveObject(int oldPosition, int newPosition) {
+        /* Create temporary copy of the object */
+        PageObject temp = pageObjects.get(oldPosition);
+        
+        /* Remove old object, Add the temporary copy at the new location */
+        pageObjects.remove(oldPosition);
+        pageObjects.add(newPosition, temp);
+        
+        //TODO Index error handling?
+    }
+    
+    /** Get page object count */
+    public int getObjectCount() {
+        return pageObjects.size();
+    }
+    
 	/** Get the page background colour */
-	public String getPageColour() {
-	    return backgroundColour;
-	}
-	
+    public String getPageColour() {
+        return backgroundColour;
+    }
+    
 	/** Set the page background colour */
-	public void setPageColour(String nBackgroundColour) {
-	    this.backgroundColour = nBackgroundColour;
-	}
+    public void setPageColour(String nBackgroundColour) {
+        this.backgroundColour = nBackgroundColour;
+    }
 }
