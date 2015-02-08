6d0d652e4f5a08ed7fe95add7d2f54ca052943a0
amj523 <amj523@york.ac.uk>
2015-02-08 16:09:12 +0000

    Formatting tabs. #21
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/data/Lesson.java
+++ TeachEasy/src/teacheasy/data/Lesson.java
@@ -15,50 +15,50 @@ import teacheasy.data.lessondata.*;
  * This class encapsulates a single Lesson
  * within the TeachEasy product.
  * 
- * @version 	1.1 08 Feb 2015
- * @author 		Alistair Jewers
+ * @version     1.1 08 Feb 2015
+ * @author      Alistair Jewers
  */
 public class Lesson {
 	
-	/** Container for the pages within this lesson */
-	public List<Page> pages;
-	
-	/** Metadata Object */
-	public LessonInfo lessonInfo;
-	
-	/** Default Settings Object */
-	public LessonDefaultSettings defaultSettings;
-	
-	/** Grade Settings Object */
+    /** Container for the pages within this lesson */
+    public List<Page> pages;
+
+    /** Metadata Object */
+    public LessonInfo lessonInfo;
+
+    /** Default Settings Object */
+    public LessonDefaultSettings defaultSettings;
+
+    /** Grade Settings Object */
 	public LessonGradeSettings gradeSettings;
-	
-	/** Constructor Method */
-	public Lesson(String author, String version, String comment,
-	              String dateCreated, int totalMarks, String lessonName,
-	              String dfBackgroundColour, String dfFont, int dfFontSize,
-	              String dfFontColour, String dfLineColour, String dfFillColour,
-	              int passMark, String passMessage, String failMessage) {
+
+    /** Constructor Method */
+    public Lesson(String author, String version, String comment,
+                  String dateCreated, int totalMarks, String lessonName,
+                  String dfBackgroundColour, String dfFont, int dfFontSize,
+                  String dfFontColour, String dfLineColour, String dfFillColour,
+                  int passMark, String passMessage, String failMessage) {
 		
-		/* Instantiate the pages container */
-		pages = new ArrayList<Page>();
+        /* Instantiate the pages container */
+        pages = new ArrayList<Page>();
+    		
+        /* Instantiate the metadata object */
+        lessonInfo = new LessonInfo(lessonName, author, version,
+                                    comment, dateCreated, totalMarks);
+
+        /* Instantiate the default settings */
+        defaultSettings = new LessonDefaultSettings(dfFontSize, dfFont,
+                                    dfBackgroundColour, dfFontColour,
+                                    dfLineColour, dfFillColour);
 		
-		/* Instantiate the metadata object */
-		lessonInfo = new LessonInfo(lessonName, author, version,
-		                            comment, dateCreated, totalMarks);
-		
-		/* Instantiate the default settings */
-		defaultSettings = new LessonDefaultSettings(dfFontSize, dfFont,
-		                            dfBackgroundColour, dfFontColour,
-		                            dfLineColour, dfFillColour);
-		
-		/* Instantiate the grade settings */
-		gradeSettings = new LessonGradeSettings(passMark, passMessage,
-		                            failMessage);
+        /* Instantiate the grade settings */
+        gradeSettings = new LessonGradeSettings(passMark, passMessage,
+                                    failMessage);
 	}
 	
-	/** Blank constructor (debug purposes only! */
-	public Lesson() {
-	    /* Instantiate the pages container */
+    /** Blank constructor (debug purposes only! */
+    public Lesson() {
+        /* Instantiate the pages container */
         pages = new ArrayList<Page>();
         
         /* Instantiate the metadata object */
