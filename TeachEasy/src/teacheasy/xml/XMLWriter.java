98e53332122bab6418b20e1fa9496667f377f31d
amj523 <amj523@york.ac.uk>
2015-02-15 12:13:24 +0000

    Began writing the xml writer. #8
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/xml/XMLWriter.java
+++ TeachEasy/src/teacheasy/xml/XMLWriter.java
@@ -6,6 +6,13 @@
  */
 package teacheasy.xml;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import teacheasy.data.Lesson;
 import teacheasy.data.Page;
 import teacheasy.data.PageObject;
@@ -22,5 +29,27 @@ public class XMLWriter {
 	/** Constructor Method */
 	public XMLWriter() {
 		
+	}
+	
+	/** Write an XML file based on a lesson data object */
+	public void writeXML(Lesson lesson) {
+	    try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            
+            Document doc = docBuilder.newDocument();
+            Element lessonElement = doc.createElement("slideshow");
+            doc.appendChild(lessonElement);
+            
+            
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	    
+	}
+	
+	public void addPage(Page page, Element lessonElement, Document doc) {
+	    
 	}
 }
