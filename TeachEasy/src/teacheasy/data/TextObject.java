b5b723bf3f43a210fcecc8fc6b071bf8ead39311
DanielBerhe <dbb503@york.ac.uk>
2015-02-17 17:30:57 +0000

    Added get & set functions for italics, bold, superscript, subscript
    Signed-off-by: DanielBerhe <dbb503@york.ac.uk>

--- TeachEasy/src/teacheasy/data/TextObject.java
+++ TeachEasy/src/teacheasy/data/TextObject.java
@@ -8,43 +8,158 @@ import java.util.ArrayList;
 
 
 public class TextObject extends PageObject {
-	private int fontSize;
-	private String fontColour;
-	private String textFont;
+	
+	private int 	fontSize;
+	private String  fontColour;
+	private String  textFont;
+	private boolean superscript;
+	private boolean subscript;
+	private boolean bold;
+	private boolean italics;
+	private boolean underline;
+	private String  textFragment;
+	
 	ArrayList <String> list; 
 	
 	public TextObject(float nXStart, float nYStart) {
 		super(PageObjectType.TEXT, nXStart, nYStart);
 		
+		list = new ArrayList <String> ();
+		list.add(textFragment);
+		
+		
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public int getFontSize() {
 		return fontSize;
 	}
-
+	
+	/**
+	 * 
+	 * @param newfontSize
+	 */
 	public void setFontSize(int newfontSize) {
 		this.fontSize = newfontSize;
 	}
-
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getFontColour() {
 		return fontColour;
 	}
-
+	
+	/**
+	 * 
+	 * @param newfontColour
+	 */
 	public void setFontColour(String newfontColour) {
 		this.fontColour = newfontColour;
 	}
-
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getTextFont() {
 		return textFont;
 	}
-
+	
+	/**
+	 * 
+	 * @param newtextFont
+	 */
 	public void setTextFont(String newtextFont) {
 		this.textFont = newtextFont;
 	}
 	
-	
-	private void storeString(){
-		list = new ArrayList <String> ();
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean get_superscript()
+	{
+		return superscript;
 	}
+	
+	/**
+	 * 
+	 * @param superscript
+	 */
+	public void set_superscript(boolean superscript)
+	{
+		this.superscript = superscript;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean get_subscript()
+	{
+		return subscript;
+	}
+	
+	/**
+	 * 
+	 * @param superscript
+	 */
+	public void set_subscript(boolean superscript)
+	{
+		this.superscript = superscript;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean get_bold(){
+		return bold;
+	}
+	
+	/**
+	 * 
+	 * @param bold
+	 */
+	public void set_bold(boolean bold){
+		this.bold = bold;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean italics(){
+		return italics;
+	}
+	
+	/**
+	 * 
+	 * @param italics
+	 */
+	public void set_italics(boolean italics){
+		this.italics = italics;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean get_underline(){
+		return underline;
+	}
+	
+	/**
+	 * 
+	 * @param underline
+	 */
+	public void set_underline(boolean underline){
+		this.underline = underline;
+	}
+	
 	
 }
