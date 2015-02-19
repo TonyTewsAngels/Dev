89121c17f2617f73056f016734bf3e1afbc3ee9e
AlexanderCash <ac1054@york.ac.uk>
2015-02-19 15:03:03 +0000

    Added AudioHandler.java file
    Signed-off-by: AlexanderCash <ac1054@york.ac.uk>

---
+++ TeachEasy/src/teacheasy/debug/AudioHandler.java
@@ -0,0 +1,32 @@
+/*
+ * Alex Cash
+ * 
+ * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
+ * 
+ */
+package teacheasy.debug;
+
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.geometry.Rectangle2D;
+
+
+/**
+ * This class provides an audio handler.
+ * 
+ * @version 	1.0 19 Feb 2015
+ * @author 		Alex Cash
+ */
+public class AudioHandler {
+
+	
+	/** Constructor Method */
+	public AudioHandler() {
+				
+	}
+
+	/** Main Function called when application is run */
+	public static void main(String[] args) {
+		new AudioHandler();
+	}
+}
