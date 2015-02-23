93bcb66088bde016fa1a5e2fddac326a91c33478
DanielBerhe <dbb503@york.ac.uk>
2015-02-23 19:26:08 +0000

    A bit of formatting

--- TeachEasy/src/teacheasy/data/ImageHandler.java
+++ TeachEasy/src/teacheasy/data/ImageHandler.java
@@ -7,44 +7,48 @@
  */
 package teacheasy.data;
 
-
 import javafx.scene.Group;
-import javafx.scene.Scene; 
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-
 public class ImageHandler {
-	
+
 	private ImageView iv1;
 
-    public void start(Stage stage) {
+	public void start(Stage stage) {
 
 		Group root = new Group();
 		Scene scene = new Scene(root, 800, 800);
-		
-		insertImage("cat.jpg", 200,200,300,90,root);
-  
-        stage.setScene(scene);
-        stage.show();
-        
-        
-    }
 
-    /**
-     * 
-     * @param imageName: name of the image as saved on disk
-     * @param locationX: desired x location of the image
-     * @param locationY: desired y location of the image
-     * @param size: actual image size multiplier. Eg, size 2 is twice as big as the original image
-     * @param rotationDegree: to rotate the image in clockwise direction
-     * @param group: group layout
-     */
-	private void insertImage(String imageName,double locationX, double locationY, double size,
-							 double rotationDegree,Group group){
+		insertImage("cat.jpg", 200, 200, 300, 90, root);
+
+		stage.setScene(scene);
+		stage.show();
+
+	}
+
+	/**
+	 * 
+	 * @param imageName
+	 *            : name of the image as saved on disk
+	 * @param locationX
+	 *            : desired x location of the image
+	 * @param locationY
+	 *            : desired y location of the image
+	 * @param size
+	 *            : actual image size multiplier. Eg, size 2 is twice as big as
+	 *            the original image
+	 * @param rotationDegree
+	 *            : to rotate the image in clockwise direction
+	 * @param group
+	 *            : group layout
+	 */
+	private void insertImage(String imageName, double locationX,
+			double locationY, double size, double rotationDegree, Group group) {
 		double imageWidth;
-		
+
 		Image image = new Image("file:" + imageName);
 		imageWidth = image.getWidth();
 		iv1 = new ImageView(image);
@@ -57,4 +61,3 @@ public class ImageHandler {
 		group.getChildren().add(iv1);
 	}
 }
-	
