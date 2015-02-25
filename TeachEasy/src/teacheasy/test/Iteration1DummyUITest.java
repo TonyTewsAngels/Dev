146bddac42d8f5028dd65e2525f1cba25421bd02
SamRaeburn <sr896@ELCLASS03.its.york.ac.uk>
2015-02-25 13:33:14 +0000

    Test checking if data is stored correctly finally complete and passes Am about to test defaults
    Signed-off-by: SamRaeburn <sr896@ELCLASS03.its.york.ac.uk>

--- TeachEasy/src/teacheasy/test/Iteration1DummyUITest.java
+++ TeachEasy/src/teacheasy/test/Iteration1DummyUITest.java
@@ -211,31 +211,31 @@ public class Iteration1DummyUITest {
 		}
 	}
 	
-//	//Check to see if defaults are stored correctly
-//	@Test
-//	public void checkDefaultStoredCorrectly() {
-//		ArrayList<String> errorList = parser.parse("/testXML/testingDefaults.xml");
-//		Lesson lesson = parser.getLesson();
-//		Page page = lesson.pages.get(0);
-//		for (int i = 0; i < page.pageObjects.size(); i++) {
-//			PageObject pageObject = page.pageObjects.get(0);
-//			
-//			assertTrue(page.getPageColour().equals("#ff00ff00"));
-//			
-//			switch (pageObject.getType()) {
-//			case TEXT:
-//				TextObject text = (TextObject)pageObject;
-//				//Check defaults have been correctly recognised
-//				assertTrue(text.getFont().equals("arial"));
-//				assertTrue(text.getFontSize() == 24);
-//				assertTrue(text.getColor().equals("#ffffffff"));
-//				
-//			default:
-//				break;
-//			}
-//		}
-//	}
+	//Check to see if defaults are stored correctly
+	@Test
+	public void checkDefaultStoredCorrectly() {
+		ArrayList<String> errorList = parser.parse("\\userfs\\sr896\\w2k\\Electronic Engineering\\3rd\\SWEng\\SWEng_workspace\\Dev\\TeachEasy\\testXML\\testingDefaults.xml");
+		Lesson lesson = parser.getLesson();
+		Page page = lesson.pages.get(0);
+		for (int i = 0; i < page.pageObjects.size(); i++) {
+			PageObject pageObject = page.pageObjects.get(i);
+
+			System.out.println(">>>>>" + page.getPageColour());
+			System.out.println(">>>>>>" + errorList);
+			assertTrue(page.getPageColour().equals("#ff00ff00"));
+			
+			switch (pageObject.getType()) {
+			case TEXT:
+				TextObject text = (TextObject)pageObject;
+				//Check defaults have been correctly recognised
+				assertTrue(text.getFont().equals("arial"));
+				assertTrue(text.getFontSize() == 24);
+				assertTrue(text.getColor().equals("#ffffffff"));
+				
+			default:
+				break;
+			}
+		}
+	}
 	
-	//Check defaults
-	//The following tests are for the writer
 }
