d6cc9a150f2c25a2c677e750b6f46d65b95a4a8b
jr829 <jr829@york.ac.uk>
2015-02-25 16:58:00 +0000

    Added AnswerBoxHandler class
    Signed-off-by: jr829 <jr829@york.ac.uk>

---
+++ TeachEasy/src/teacheasy/data/AnswerBoxHandler.java
@@ -0,0 +1,17 @@
+package teacheasy.data;
+
+import java.awt.TextField;
+
+public class AnswerBoxHandler {
+
+	
+	public void createAnswerBox (int nXStart, int nYStart, int nCharacterLimit,
+			   int nMarks, String nCorrectAnswers, boolean nRetry) {
+		
+		TextField answerField = new TextField();
+		answerField.setLocation(nXStart, nYStart);
+	
+		
+	}
+	
+}
