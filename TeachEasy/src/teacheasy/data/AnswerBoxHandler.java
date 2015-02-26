05cb2808b103734aee6ea7841f509cc69af8d323
jr829 <jr829@york.ac.uk>
2015-02-26 16:57:19 +0000

    edited answerBoxHandler, added: answerchecker
    Signed-off-by: jr829 <jr829@york.ac.uk>

--- TeachEasy/src/teacheasy/data/AnswerBoxHandler.java
+++ TeachEasy/src/teacheasy/data/AnswerBoxHandler.java
@@ -5,29 +5,54 @@ package teacheasy.data;
 
 import java.awt.TextField;
 
-
-
 /**
  * 
  * @author Daniel Berhe and Jake Ransom
  * @version 1.0
- *
+ * 
  */
 public class AnswerBoxHandler {
-	
-	public AnswerBoxHandler(){
-		
+
+	TextField answerField;
+	public int marks;
+	boolean retry;
+	String correctAnswers;
+
+	public AnswerBoxHandler(String nCorrectAnswers) {
+
+		answerField = new TextField();
+		marks = 0;
+		this.correctAnswers = nCorrectAnswers;
+
 	}
 
-	
-	public void createAnswerBox (int nXStart, int nYStart, int nCharacterLimit,
-			   int nMarks, String nCorrectAnswers, boolean nRetry) {
-		
-		TextField answerField = new TextField();
+	public void createAnswerBox(int nXStart, int nYStart, int nCharacterLimit,
+			boolean nRetry) {
+
 		answerField.setLocation(nXStart, nYStart);
 		answerField.setColumns(nCharacterLimit);
-	
-		
+		this.retry = nRetry;
+
 	}
-	
+
+	public boolean answerChecker(int nMarks) {
+
+		boolean isCorrect = false;
+
+		String[] listOfCorrectAnswers = correctAnswers.split("-");
+
+		for (int i = 0; i < listOfCorrectAnswers.length; i++) {
+
+			if (answerField.getText() == listOfCorrectAnswers[i]) {
+				isCorrect = true;
+				marks += nMarks;
+			} else {
+				isCorrect = false;
+
+			}
+		}
+		answerField.setEditable(retry);
+		return isCorrect;
+
+	}
 }
