e08350ee842cbaed14b8948de0a2c3982c662a60
EMMANUEL <epo500@york.ac.uk>
2015-03-05 16:10:26 +0000

    Tidied up code and made sure radio buttons work fine

--- TeachEasy/src/teacheasy/mediahandler/multichoice/multipleChoice.java
+++ TeachEasy/src/teacheasy/mediahandler/multichoice/multipleChoice.java
@@ -33,6 +33,7 @@ public class multipleChoice{
 	ArrayList<MChoiceRadio> rB; 
 
 
+
 	public multipleChoice (Group nGroup , ArrayList<Answer>answers, int defaultPadding, 
 			int spacing, MultiChoiceType type, Orientation orientation ) {
 
@@ -46,49 +47,56 @@ public class multipleChoice{
 
 		// horizontal position 
 		HBox horizontalPosition = new HBox(spacing);
-		horizontalPosition.setPadding( new Insets (defaultPadding));
-		
+		horizontalPosition.setPadding(new Insets (defaultPadding));
+
 		//initialise mark button
 		Button markButton = new Button("Mark");
+
+		// To be Able to choose Only one radio option
+		ToggleGroup tGroup = new ToggleGroup();
 
 		switch (type){
 
 		case CHECKBOX:
 			//Creation of CheckBox's 
 			for (int i = 0; i < answers.size();i++ ){
-
 				CheckBox temp = new CheckBox(answers.get(i).getText());
 				cB.add(new MChoiceCheckBox(temp, answers.get(i).isCorrect()));
-
-			}
-
+			} 
 			/*check which box has been selected 
-			set orientation 
-		    creating multiple vertical check boxes */
-
+			    				    set orientation 
+			    			        creating multiple vertical check boxes */
 			if (orientation == Orientation.VERTICAL){
 				for(int i= 0; i < cB.size(); i++ ){
 					verticalPosition.getChildren().add(cB.get(i).getcB());					
 				}
 				verticalPosition.getChildren().add(markButton);
 				group.getChildren().add(verticalPosition);
-			}else 
+			}else {
 				//create multiple horizontal check boxes
 				for(int i = 0; i < cB.size(); i++ ){
 					horizontalPosition.getChildren().add(cB.get(i).getcB());
-					horizontalPosition.getChildren().add(markButton);
+				}
+			horizontalPosition.getChildren().add(markButton);
+			group.getChildren().add(horizontalPosition);
+		    }
+
+			markButton.setOnAction(new EventHandler<ActionEvent>(){
+				@Override 
+				public void handle (ActionEvent event){
+					boolean correctCheckBoxAnswer = Check();
+					if (correctCheckBoxAnswer){
+						System.out.println("Thats Great ");
+					}else 
+						System.out.println("Try again");
 				}
 
+			});
 
 			break;
 
 		case RADIO:
-
-			// To be Able to choose Only one radio option
-			ToggleGroup tGroup = new ToggleGroup();
-
 			//creation of radio buttons 
-
 			for(int i = 0; i < answers.size(); i++ ){
 				RadioButton tempRb = new RadioButton(answers.get(i).getText());
 				tempRb.setToggleGroup(tGroup);
@@ -96,47 +104,73 @@ public class multipleChoice{
 			}
 
 			//set orientation 
-			if( orientation == Orientation.VERTICAL)
-
+			if( orientation == Orientation.VERTICAL){
 				// creating multiple vertical radio buttons  
 				for(int i= 0; i < rB.size(); i++ ){
 					verticalPosition.getChildren().add(rB.get(i).getrB());
-				}else 
-					// create multiple horizontal radio buttons 
-					for(int i =0; i< rB.size();i++){
-						horizontalPosition.getChildren().add(rB.get(i).getrB());
-					}
+				}
+				
+				verticalPosition.getChildren().add(markButton);
+				group.getChildren().add(verticalPosition);
+			} else { 
+				// create multiple horizontal radio buttons 
+				for(int i =0; i< rB.size();i++){
+					horizontalPosition.getChildren().add(rB.get(i).getrB());
+				}
+				
+				horizontalPosition.getChildren().add(markButton);
+				group.getChildren().add(horizontalPosition);
+			}
+
+			markButton.setOnAction(new EventHandler<ActionEvent>(){
+				@Override 
+				public void handle (ActionEvent event){
+					boolean correctRadioAnswer = RadioCheck();
+					if (correctRadioAnswer){
+						System.out.println("thats awesome ");
+					}else 
+						System.out.println("you need more help");
+				}
+			});
 			break;
+
 		case DROPDOWNLIST:
 			break;
-			
-			//add action listener for button so it could check the answers are correct
 
 
 		}
-       
-        markButton.setOnAction(new EventHandler<ActionEvent>(){
-        	@Override 
-        	public void handle (ActionEvent event){
-        		
-        		boolean correctAnswer = check();
-        		
-        		if (correctAnswer){
-        			System.out.println("Thats Great ");
-        		}else 
-        			System.out.println("Try again");
-        	}
-        });
+
+
+		//horizontalPosition.getChildren().add(markButton);
+
+
+		/* markButton.setOnAction(new EventHandler<ActionEvent>(){
+		        	@Override 
+		        	public void handle (ActionEvent event){
+		        		boolean correctCheckBoxAnswer = Check();
+		        		boolean correctRadioAnswer = RadioCheck();
+
+		        		     if (correctCheckBoxAnswer){
+		        			 System.out.println("Thats Great ");
+		        		     }else 
+		        			 System.out.println("Try again");
+
+		        	         if (correctRadioAnswer){
+		        		     System.out.println("thats awesome ");
+		        	         }else 
+		        		     System.out.println("you need more help");
+		                     }
+		        });*/
 
 	}
-	
-	public boolean check(){
-		
+
+	public boolean Check(){
+
 		for(int i = 0; i < cB.size(); i++ ){
-			
-            //checks if the check boxes have been selected 
+
+			//checks if the check boxes have been selected 
 			if(cB.get(i).getcB().isSelected()){
-				
+
 				/*checks if the right answer has been selected and returns false 
 				if the right answer has not been selected */
 				if(!cB.get(i).isCorrect()){
@@ -149,22 +183,25 @@ public class multipleChoice{
 				}
 			}
 		}
-		
+
 		return true;
 	}
-	
- public boolean radioCheck(){
-	 
-	 for(int i = 0; i < rB.size(); i++){
-		 if(rB.get(i).getrB().isSelected()){
-			 if (!rB.get(i).isCorrect()){
-				 return false;
-			 }
-		 }
-		 
-	 } return true; 
-	 
- }
+
+	public boolean RadioCheck(){
+		/* */
+		for(int i = 0; i < rB.size(); i++){
+			/* */
+			if(rB.get(i).getrB().isSelected()){
+				/* */
+				if (!rB.get(i).isCorrect()){
+					return false;
+				}
+			}
+		}
+		
+		/* */
+		return true; 
+	}
 
 
 	public static void main(String[] args) {
