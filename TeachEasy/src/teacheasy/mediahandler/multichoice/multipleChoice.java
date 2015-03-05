bf69125cb689b0b5d276efd9b9b5272d676e71b4
EMMANUEL <epo500@york.ac.uk>
2015-03-05 02:03:35 +0000

    wrote code to check which answer boxes are ticked and created a button to mark the work
    Signed-off-by: EMMANUEL <epo500@york.ac.uk>

--- TeachEasy/src/teacheasy/mediahandler/multichoice/multipleChoice.java
+++ TeachEasy/src/teacheasy/mediahandler/multichoice/multipleChoice.java
@@ -4,12 +4,18 @@ import java.awt.List;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.swing.event.ChangeListener;
+
 import teacheasy.data.MultipleChoiceObject.MultiChoiceType;
 import teacheasy.data.MultipleChoiceObject.Orientation;
 import teacheasy.data.multichoice.Answer;
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
@@ -23,9 +29,9 @@ import javafx.stage.Stage;
 public class multipleChoice{
 
 	private Group group ;
-
 	ArrayList<MChoiceCheckBox> cB;
 	ArrayList<MChoiceRadio> rB; 
+
 
 	public multipleChoice (Group nGroup , ArrayList<Answer>answers, int defaultPadding, 
 			int spacing, MultiChoiceType type, Orientation orientation ) {
@@ -41,35 +47,48 @@ public class multipleChoice{
 		// horizontal position 
 		HBox horizontalPosition = new HBox(spacing);
 		horizontalPosition.setPadding( new Insets (defaultPadding));
+		
+		//initialise mark button
+		Button markButton = new Button("Mark");
 
 		switch (type){
 
 		case CHECKBOX:
 			//Creation of CheckBox's 
 			for (int i = 0; i < answers.size();i++ ){
+
 				CheckBox temp = new CheckBox(answers.get(i).getText());
 				cB.add(new MChoiceCheckBox(temp, answers.get(i).isCorrect()));
+
 			}
 
-			//set orientation 
+			/*check which box has been selected 
+			set orientation 
+		    creating multiple vertical check boxes */
 
-			// creating multiple vertical check boxes 
 			if (orientation == Orientation.VERTICAL){
 				for(int i= 0; i < cB.size(); i++ ){
-					verticalPosition.getChildren().add(cB.get(i).getcB());
+					verticalPosition.getChildren().add(cB.get(i).getcB());					
 				}
+				verticalPosition.getChildren().add(markButton);
+				group.getChildren().add(verticalPosition);
 			}else 
 				//create multiple horizontal check boxes
-				for(int i= 0; i < cB.size(); i++ ){
+				for(int i = 0; i < cB.size(); i++ ){
 					horizontalPosition.getChildren().add(cB.get(i).getcB());
+					horizontalPosition.getChildren().add(markButton);
 				}
+
 
 			break;
 
 		case RADIO:
+
 			// To be Able to choose Only one radio option
 			ToggleGroup tGroup = new ToggleGroup();
+
 			//creation of radio buttons 
+
 			for(int i = 0; i < answers.size(); i++ ){
 				RadioButton tempRb = new RadioButton(answers.get(i).getText());
 				tempRb.setToggleGroup(tGroup);
@@ -90,33 +109,62 @@ public class multipleChoice{
 			break;
 		case DROPDOWNLIST:
 			break;
+			
+			//add action listener for button so it could check the answers are correct
+
 
 		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+       
+        markButton.setOnAction(new EventHandler<ActionEvent>(){
+        	@Override 
+        	public void handle (ActionEvent event){
+        		
+        		boolean correctAnswer = check();
+        		
+        		if (correctAnswer){
+        			System.out.println("Thats Great ");
+        		}else 
+        			System.out.println("Try again");
+        	}
+        });
 
 	}
-
+	
+	public boolean check(){
+		
+		for(int i = 0; i < cB.size(); i++ ){
+			
+            //checks if the check boxes have been selected 
+			if(cB.get(i).getcB().isSelected()){
+				
+				/*checks if the right answer has been selected and returns false 
+				if the right answer has not been selected */
+				if(!cB.get(i).isCorrect()){
+					return false;
+				}
+				/*if the right answer has then been */
+			} else {
+				if(cB.get(i).isCorrect()){
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+ public boolean radioCheck(){
+	 
+	 for(int i = 0; i < rB.size(); i++){
+		 if(rB.get(i).getrB().isSelected()){
+			 if (!rB.get(i).isCorrect()){
+				 return false;
+			 }
+		 }
+		 
+	 } return true; 
+	 
+ }
 
 
 	public static void main(String[] args) {
