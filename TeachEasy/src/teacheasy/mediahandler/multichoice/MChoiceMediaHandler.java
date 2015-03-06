4198df294be68982ae0a1e48639a3dbdd0a634d0
EMMANUEL <epo500@york.ac.uk>
2015-03-06 05:08:39 +0000

    done the multiple choice media handler #51
    Signed-off-by: EMMANUEL <epo500@york.ac.uk>

--- TeachEasy/src/teacheasy/mediahandler/multichoice/MChoiceMediaHandler.java
+++ TeachEasy/src/teacheasy/mediahandler/multichoice/MChoiceMediaHandler.java
@@ -1,5 +1,32 @@
 package teacheasy.mediahandler.multichoice;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import teacheasy.data.MultipleChoiceObject.MultiChoiceType;
+import teacheasy.data.MultipleChoiceObject.Orientation;
+import teacheasy.data.multichoice.Answer;
+import javafx.scene.Group;
+
 public class MChoiceMediaHandler {
+	private Group group; 
+	private List <MultipleChoice> multipleChoice;
+	
+	/*constructor method */
+	
+	public MChoiceMediaHandler(Group nGroup) {
+		
+		this.group = nGroup;
+		
+		/* initialise the Question list */
+		this.multipleChoice = new ArrayList<MultipleChoice>();
+		
+	}
+	public void createMultipleChoice(Group nGroup , ArrayList<Answer>answers, int defaultPadding, 
+			int spacing, MultiChoiceType type, Orientation orientation){
+		
+	multipleChoice.add(new MultipleChoice(group, answers, defaultPadding, spacing, type , 
+			      orientation));
+	}
 
 }
