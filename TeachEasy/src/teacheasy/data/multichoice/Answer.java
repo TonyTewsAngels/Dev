b521024e17d61bd40c228c0f902b70b488ec8829
ajewers <amj523@york.ac.uk>
2015-03-06 14:12:22 +0000

    Added copyright and class comments to answer class
    #60

--- TeachEasy/src/teacheasy/data/multichoice/Answer.java
+++ TeachEasy/src/teacheasy/data/multichoice/Answer.java
@@ -1,5 +1,15 @@
+/*
+ * Copyright (c) sofia Software Solutions, All Rights Reserved.
+ */
 package teacheasy.data.multichoice;
 
+/**
+ * This class encapsulates one answer within
+ * a multiple choice set.
+ * 
+ * @author Alistair Jewers
+ * @version 1.0 06 Feb 2015
+ */
 public class Answer {
     private String text;
     private boolean correct;
