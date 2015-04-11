76536f0af173b39378a7a4e13adb07e2695ee301
amj523 <amj523@york.ac.uk>
2015-04-11 09:55:20 +0100

    Started adding a content handler for the Image element. Changes are needed to the Image data object and the XML structure. #99
    Signed-off-by: amj523 <amj523@york.ac.uk>

---
+++ TeachEasy/src/teacheasy/xml/contenthandlers/ImageXMLHandler.java
@@ -0,0 +1,59 @@
+package teacheasy.xml.contenthandlers;
+
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+import teacheasy.data.ImageObject;
+import teacheasy.data.Lesson;
+import teacheasy.data.Page;
+import teacheasy.xml.XMLElement;
+import teacheasy.xml.util.XMLNotification;
+
+public class ImageXMLHandler extends DefaultHandler{
+    private Lesson lesson;
+    private Page page;
+    private ImageObject image;
+    
+    private ArrayList<XMLNotification> errorList;
+    private XMLReader xmlReader;
+    private DefaultHandler parent;
+    
+    public ImageXMLHandler(XMLReader nXMLReader, DefaultHandler nParent, Lesson nLesson, Page nPage, ArrayList<XMLNotification> nErrorList, Attributes slideAttrs) {
+        this.xmlReader = nXMLReader;
+        this.parent = nParent;
+        
+        this.lesson = nLesson;
+        this.page = nPage;
+        this.errorList = nErrorList;
+        
+        image = new ImageObject(0.0f, 0.0f, null, 0.0f, 0.0f, 0.0f);
+        
+        imageStart(slideAttrs);
+    }
+    
+    public void endElement(String uri, String localName, String qName) {
+        switch(XMLElement.check(qName.toUpperCase())) {
+            /* If the image element has finished, return to the parent */
+            case IMAGE:
+                endHandler();
+                break;            
+            default:
+                break;
+        }
+    }
+    
+    public void endHandler() {
+        page.pageObjects.add(image);
+        
+        xmlReader.setContentHandler(parent);
+    }
+    
+    public void imageStart(Attributes attrs) {
+        String sourceFileStr = attrs.getValue("sourcefile");
+        String xStartStr = attrs.getValue("xstart");
+        String yStartStr = attrs.getValue("ystart");
+    }
+}
