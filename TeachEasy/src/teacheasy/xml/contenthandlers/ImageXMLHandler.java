9122840c99512b0faa916a7279e85ef0a7d1ead8
amj523 <amj523@york.ac.uk>
2015-04-16 11:32:43 +0100

    Started changing image object #99

--- TeachEasy/src/teacheasy/xml/contenthandlers/ImageXMLHandler.java
+++ TeachEasy/src/teacheasy/xml/contenthandlers/ImageXMLHandler.java
@@ -11,6 +11,7 @@ import teacheasy.data.Lesson;
 import teacheasy.data.Page;
 import teacheasy.xml.XMLElement;
 import teacheasy.xml.util.XMLNotification;
+import teacheasy.xml.util.XMLNotification.*;
 
 public class ImageXMLHandler extends DefaultHandler{
     private Lesson lesson;
@@ -46,6 +47,8 @@ public class ImageXMLHandler extends DefaultHandler{
     }
     
     public void endHandler() {
+        
+        
         page.pageObjects.add(image);
         
         xmlReader.setContentHandler(parent);
@@ -61,5 +64,54 @@ public class ImageXMLHandler extends DefaultHandler{
         String rotationStr = attrs.getValue("scale");
         String startTimeStr = attrs.getValue("starttime");
         String durationStr = attrs.getValue("duration");
+        
+        float xStart = checkFloat(xStartStr, 0.0f, Level.ERROR,
+                "Page " + lesson.pages.size() + ", Object " + page.getObjectCount() +" (Image) Missing X Start.");
+        
+        float yStart = checkFloat(yStartStr, 0.0f, Level.ERROR,
+                "Page " + lesson.pages.size() + ", Object " + page.getObjectCount() +" (Image) Missing Y Start.");
+        
+        float scale = checkFloat(scaleStr, 1.0f, Level.WARNING,
+                "Page " + lesson.pages.size() + ", Object " + page.getObjectCount() +" (Image) No scale attribute, default 1 inserted.");
+        
+        float xEnd = checkFloat(xEndStr, -1.0f, Level.WARNING,
+                "Page " + lesson.pages.size() + ", Object " + page.getObjectCount() +" (Image) No X End attribute, placeholder -1 inserted. Use scale for size.");
+        
+        float yEnd = checkFloat(yEndStr, -1.0f, Level.WARNING,
+                "Page " + lesson.pages.size() + ", Object " + page.getObjectCount() +" (Image) No Y End attribute, placeholder -1 inserted. Use scale for size.");
+        
+        float rotation = checkFloat(rotationStr, 0.0f, Level.WARNING,
+                "Page " + lesson.pages.size() + ", Object " + page.getObjectCount() +" (Image) No rotation attribute, default 0 inserted.");
+        
+        float startTime = checkFloat(startTimeStr, 0.0f, Level.WARNING,
+                "Page " + lesson.pages.size() + ", Object " + page.getObjectCount() +" (Image) No start time attribute, default 0 inserted.");
+        
+        float duration = checkFloat(durationStr, 0.0f, Level.WARNING,
+                "Page " + lesson.pages.size() + ", Object " + page.getObjectCount() +" (Image) No duration attribute, default 0 inserted.");
+    }
+    
+    public float checkFloat(String str, float defaultValue, Level errorLevel, String errorMessage) {
+        float val;
+        
+        try {
+            val = Float.parseFloat(str);
+        } catch (NumberFormatException e) {
+            errorList.add(new XMLNotification(errorLevel, errorMessage));
+            val = defaultValue;
+        }
+        
+        return val;
+    }
+    
+    public float checkFloat(String str, float defaultValue) {
+        float val;
+        
+        try {
+            val = Float.parseFloat(str);
+        } catch (NumberFormatException e) {
+            val = defaultValue;
+        }
+        
+        return val;
     }
 }
