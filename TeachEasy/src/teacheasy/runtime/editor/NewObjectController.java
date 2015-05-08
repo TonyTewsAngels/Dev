9ab56717265dceb796085ee1988708ad3cce46ee
amj523 <amj523@york.ac.uk>
2015-05-08 21:55:35 +0100

    Stashing object adding changes #116
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/runtime/editor/NewObjectController.java
+++ TeachEasy/src/teacheasy/runtime/editor/NewObjectController.java
@@ -1,5 +1,41 @@
 package teacheasy.runtime.editor;
 
-public class NewObjectController {
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import teacheasy.data.Page;
+import teacheasy.data.PageObject.PageObjectType;
 
+public class NewObjectController {
+    public static void addObject(Page page, PageObjectType type) {
+        switch(type) {
+            case ANSWER_BOX:
+                
+                break;
+            case AUDIO:
+                break;
+            case BUTTON:
+                break;
+            case GRAPHIC:
+                break;
+            case IMAGE:
+                addImageObject(page);
+                break;
+            case MULTIPLE_CHOICE:
+                break;
+            case TEXT:
+                break;
+            case VIDEO:
+                break;
+            default:
+                break;
+        }
+    }
+    
+    public static void addImageObject(Page page) {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Open Image");
+        fc.showOpenDialog(new Stage());
+        
+        
+    }
 }
