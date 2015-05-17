646775c3b234c87e7348dc65e291d17fe464d73a
lt669 <lt669@york.ac.uk>
2015-05-17 17:24:16 +0100

    Started re-skinning buttons and adding MouseListeners to change the button images.
    Signed-off-by: lt669 <lt669@york.ac.uk>

--- TeachEasy/src/teacheasy/debug/TE_GUI_3.java
+++ TeachEasy/src/teacheasy/debug/TE_GUI_3.java
@@ -9,11 +9,15 @@
 package teacheasy.debug;
 
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
 import javafx.event.*;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
@@ -42,6 +46,7 @@ import javafx.*;
 public class TE_GUI_3 extends Application {
 
 Text text, botText, propText, titleText;
+Image textIm;
 	
 	/**
      * Override the start method inside the JavaFX Application
@@ -71,7 +76,7 @@ Text text, botText, propText, titleText;
         
         /* Set layout colours */
         topGrid.setStyle("-fx-background-color: Grey;");
-        propertiesPanel.setStyle("-fx-background-color: yellow;");
+        propertiesPanel.setStyle("-fx-background-color:  rgb(177, 177, 177);");
         contentPanel.setStyle("-fx-background-color: rgb(101, 4, 7);");
         bottomBar.setStyle("-fx-background-color: Grey;");
         innerGrid.setStyle("-fx-background-color: rgb(101, 4, 7);");
@@ -80,9 +85,8 @@ Text text, botText, propText, titleText;
         Menu menuFile = new Menu("File");
         Menu menuEdit = new Menu("Edit");
         Menu menuHelp = new Menu("Help");
-        Menu menuOption = new Menu("Cunt Button");
         menuBar.setPrefWidth(10000);
-        menuBar.getMenus().addAll(menuFile, menuEdit, menuHelp, menuOption);
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuHelp);
         
         
         /* DUMMY CONTENT */
@@ -117,14 +121,129 @@ Text text, botText, propText, titleText;
         
         Button butt = new Button("Dummy");
         
-        Button text = new Button("Text");
-        Button image = new Button("Image");
-        Button video = new Button("Video");
-        Button tick = new Button("Ticks");
-        Button graphic = new Button("Graphics");
-        Button audio = new Button("Audio");
-        
+        Button text = new Button();
+        text.setStyle("-fx-background-color: transparent;");
+        Button image = new Button();
+        Button video = new Button();
+        Button tick = new Button();
+        Button graphic = new Button();
+        Button audio = new Button();
+     
         Button prev = new Button("Preview");
+        
+        /*Button dimensions*/
+        
+     
+        
+        
+        /* Create page tabs */
+        
+        /* Re-skin buttons */
+      
+   
+        int x = 40;
+        int y = 40;
+        
+        /* Images */
+        
+        //Text Box
+        Image textImST = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image textImHO = new Image(getClass().getResourceAsStream("Textbox_HO_TOP_CIRC_Blue_T-01.png"));
+        Image textImPR = new Image(getClass().getResourceAsStream("Textbox_PRE_TOP_CIRC_Blue_T-01.png"));
+        
+        //Image
+        Image imImST = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image imImHO = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image imImPR = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        
+        //Video
+        Image vidImST = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image vidImHO = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image vidImPR = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        
+        //Audio
+        Image audioImST = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image audioImHO = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image audioImPR = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        
+        //Graphics
+        Image graphicImST = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image graphicImHO = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image graphicImPR = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        
+        //Question Box
+        Image queImST = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image queImHO = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image queImPR = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        
+        //Multiple Choice
+        Image mulImST = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image mulImHO = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        Image mulImPR = new Image(getClass().getResourceAsStream("Textbox_ST_TOP_CIRC_Blue_T-01.png"));
+        
+        /* Image Views */
+        ImageView textBox = new ImageView(textImST);
+        
+        
+        
+        /* Enent Handlers */
+        final InvalidationListener hoverListener = new InvalidationListener() {
+            public void invalidated(Observable ov) {
+               if (textBoxST.isHover()) {
+            	   ImageView textBox = new ImageView(textImHO);
+                } else {
+                    text.setText("not hovered");
+              }
+            }
+        }
+        
+        ImageView textBoxHO = new ImageView(textImHO);
+        ImageView textBoxPR= new ImageView(textImPR);
+        
+       /* ImageView imImST = new ImageView(imImST);
+        ImageView imImHO = new ImageView(imImHO);
+        ImageView imImPR= new ImageView(imImPR);
+
+        ImageView vidImST = new ImageView(vidImST);
+        ImageView vidImHO = new ImageView(vidImHO);
+        ImageView vidImPR= new ImageView(vidImPR);
+        
+        ImageView audioImST = new ImageView(textImST);
+        ImageView audioImHO = new ImageView(audioImHO);
+        ImageView audioImPR= new ImageView(audioImPR);
+        
+        ImageView graphicImST = new ImageView(textImST);
+        ImageView graphicImHO = new ImageView(graphicImHO);
+        ImageView graphicImPR= new ImageView(graphicImPR);
+        
+        ImageView queImST = new ImageView(textImST);
+        ImageView queImPR = new ImageView(queImPR);
+        ImageView queImHO= new ImageView(queImHO);
+        
+        ImageView mulImST = new ImageView(mulImST);
+        ImageView mulImHO = new ImageView(mulImHO);
+        ImageView mulImPR= new ImageView(mulImPR);
+        */
+        
+        text.setGraphic(textBoxST); 
+
+        textBoxST.setFitWidth(x);
+        textBoxST.setFitHeight(y);
+        
+        
+       /* Image textIm = new Image("Textbox_ST_TOP_CIRC_Blue_T-01.png");
+        ImageView textBox = new ImageView();
+        textBox.setImage(textIm);
+        text.setGraphic(textBox);*/
+        
+
+        
+        
+      
+        
+textBox.setOnMousePressed(new EventHandler<MouseEvent>(){
+	public void handle(MouseEvent me) {textBoxPressed();}
+});
         
         
         
