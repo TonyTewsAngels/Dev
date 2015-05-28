bf6f3ea86e644c6bca3fe7ba83d6bc3fdcb9a89c
AlexanderCash <ac1054@york.ac.uk>
2015-05-28 15:12:43 +0100

    #161 copied master copy of audiohandler dummy gui to enable merge

--- TeachEasy/src/teacheasy/debug/AudioHandlerDummyGUI.java
+++ TeachEasy/src/teacheasy/debug/AudioHandlerDummyGUI.java
@@ -2,7 +2,6 @@ package teacheasy.debug;
 
 import java.io.File;
 
-import teacheasy.debug.ImageHandlerDummyGUI.buttonEventHandler;
 import teacheasy.mediahandler.AudioHandler;
 import teacheasy.mediahandler.audio.Audio;
 import javafx.application.Application;
@@ -28,56 +27,20 @@ public class AudioHandlerDummyGUI extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         
-        
-        /* Create a button */
-        Button btn = new Button();
-        
-        /* Setup the button */
-        btn.setText("A Button");
-        btn.setId("Button1");
-        btn.relocate(200.0, 50.0);
-        
-        /* Set the button to use the button event handler */
-        btn.setOnAction(new buttonEventHandler());
-        
-        /* Add the button to the group */
-        group.getChildren().addAll(btn);
-        
 
         /* Instantiate the audio handler */
         audioHandler = new AudioHandler(group);
 
         String sourceFile = new String("http://www.tonycuffe.com/mp3/tail%20toddle.mp3");
 
-        //audioHandler.createAudio(50, 50, 10, sourceFile, false, true, false, true);
-        audioHandler.createAudio(50, 200, 200, sourceFile, false, true, true, false);
-        audioHandler.createAudio(300, 200, 500, sourceFile, false, false, true, true);
-        audioHandler.createAudio(50, 400, 200, sourceFile, false, false, false, true);
-        
-        
+        audioHandler.createAudio(50, 50, 10, sourceFile, false, true, true, false);
+        //audioHandler.createAudio(50, 200, 500, sourceFile, true, false, true, true);
+        //audioHandler.createAudio(50, 400, 200, sourceFile, false, true, false, true);
 
         
         /* Show the window */
         primaryStage.show(); 
     }
-    
-    /**
-     * Event Handler Class
-     */
-    public class buttonEventHandler implements EventHandler<ActionEvent> {
-        @Override
-        public void handle(ActionEvent e) {
-        	System.out.println("xEnd0 is:" +audioHandler.getAudioXEnd(0));
-        	System.out.println("yEnd0 is:" +audioHandler.getAudioYEnd(0));
-        	
-        	System.out.println("xEnd1 is:" +audioHandler.getAudioXEnd(1));
-        	System.out.println("yEnd1 is:" +audioHandler.getAudioYEnd(1));
-        	
-        	System.out.println("xEnd2 is:" +audioHandler.getAudioXEnd(2));
-        	System.out.println("yEnd2 is:" +audioHandler.getAudioYEnd(2));
-            }
-        }
-      
     
     public static void main(String args[]) {
         launch();
