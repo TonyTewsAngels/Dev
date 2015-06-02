6733ea768e94242fbe8b7aac8497af9ba9ec75d6
DanielBerhe <dbb503@york.ac.uk>
2015-06-02 11:05:01 +0100

    Finished commenting MChoiceRadio class. #247
    Signed-off-by: DanielBerhe <dbb503@york.ac.uk>

--- TeachEasy/src/teacheasy/mediahandler/multichoice/MChoiceRadio.java
+++ TeachEasy/src/teacheasy/mediahandler/multichoice/MChoiceRadio.java
@@ -9,35 +9,63 @@ package teacheasy.mediahandler.multichoice;
 import javafx.scene.control.RadioButton;
 
 /**
- * A class to label right and wrong radio buttons 
+ * A class to label right and wrong radio buttons
  * 
- * @author	Emmanuel Olutayo
- * @version	1.0 07 Mar 2015
+ * @author Emmanuel Olutayo
+ * @version 1.0 07 Mar 2015
  */
 public class MChoiceRadio {
-    
-	private RadioButton radioButton;
-	private boolean correct;
 
-	public MChoiceRadio (RadioButton nRb, boolean nCorrect ){
-		this.radioButton = nRb;
-		this.correct =nCorrect;
-	}
+    private RadioButton radioButton;
+    private boolean correct;
 
-	public RadioButton getRadioButton() {
-		return radioButton;
-	}
+    /**
+     * 
+     * @param nRb
+     *            RadioButton object which is to be labelled
+     * @param nCorrect
+     *            <code>true</code> if object answered correctly;
+     *            <code>false</code> otherwise.
+     */
+    public MChoiceRadio(RadioButton nRb, boolean nCorrect) {
+        this.radioButton = nRb;
+        this.correct = nCorrect;
+    }
 
-	public void setRadioButton(RadioButton nrB) {
-		this.radioButton = nrB;
-	}
+    /**
+     * 
+     * @return RadioButton object
+     */
+    public RadioButton getRadioButton() {
+        return radioButton;
+    }
 
-	public boolean isCorrect() {
-		return correct;
-	}
+    /**
+     * 
+     * @param nrB
+     *            RadioButton object
+     */
+    public void setRadioButton(RadioButton nrB) {
+        this.radioButton = nrB;
+    }
 
-	public void setCorrect(boolean ncorrect) {
-		this.correct = ncorrect;
-	}
+    /**
+     * 
+     * @return <code>true</code> if answer is correct; <code>false</code>
+     *         otherwise.
+     */
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    /**
+     * Sets the boolean variable correct
+     * 
+     * @param ncorrect
+     *            Desired boolean value for the variable correct
+     */
+    public void setCorrect(boolean ncorrect) {
+        this.correct = ncorrect;
+    }
 
 }
