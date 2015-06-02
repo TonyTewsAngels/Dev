952fe0553de4992e26a3e33cbb058c0ea732ec58
amj523 <amj523@york.ac.uk>
2015-06-02 11:00:33 +0100

    Commenting and refactoring audio and image handler #247
    Signed-off-by: amj523 <amj523@york.ac.uk>

--- TeachEasy/src/teacheasy/mediahandler/ImageHandler.java
+++ TeachEasy/src/teacheasy/mediahandler/ImageHandler.java
@@ -1,4 +1,5 @@
 /*
+ * Daniel Berhe & Jake Ransom
  *
  * Copyright (c) 2015 Sofia Software Solutions. All Rights Reserved.
  */
@@ -15,73 +16,86 @@ import javafx.scene.image.ImageView;
 
 /**This class inserts an image with specified image name, size, location(x and y) and rotation degrees.
  * 
- * @Authors: Daniel Berhe & Jake Ransom
- * @Verion:  1.0 23/02/2015
+ * @Author Daniel Berhe 
+ * @author Jake Ransom
+ * @Verion 1.0 23 Feb 2015
  * 
- * */
+ */
 public class ImageHandler {
+    
+    /* Reference to the group to add images to */
 	private Group group;
+	
+	/* Array list of the image being handled */
 	private ArrayList<ImageView> images;
 
+	/**
+	 * Constructor for the image handler.
+	 * 
+	 * @param nGroup The group to add images to.
+	 */
 	public ImageHandler(Group nGroup){
+	    /* Set up the group reference */
 		group = nGroup;
+		
+		/* Instantiate the list of images */
 		images = new ArrayList<ImageView>();
 	}
 
 	/**
+	 * Adds a new image to the group.
 	 * 
-	 * @param imageName
-	 *            : name of the image as saved on disk
-	 * @param locationX
-	 *            : desired x location of the image
-	 * @param locationY
-	 *            : desired y location of the image
-	 * @param widthSize
-	 *            : actual image with multiplier. Eg, size 2 is twice as big as
-	 *            the original image
-	 * @param heightSize: actual image height multiplier
-	 * @param rotationDegree
-	 *            : to rotate the image in clockwise direction
-	 * @param group
-	 *            : group layout
+	 * @param imageName Name of the image as saved on disk.
+	 * @param locationX Desired x axis position of the image.
+	 * @param locationY Desired y axis position of the image.
+	 * @param widthSize Actual image width multiplier. Eg, size 
+	 *                  2 is twice as big as the original image.
+	 * @param heightSize Actual image height multiplier.
+	 * @param rotationDegree Rotation of the image in degrees.
+	 * @param scaleBased Indicates if the size parameters are 
+	 *                   scale based or absolute.
 	 */
 	public void insertImage(String imageName, double locationX,
 			                double locationY, double widthSize,
 			                double heightSize, double rotationDegree,
 			                boolean scaleBased) {
+	    /* Size parameters */
 		double imageWidth;
 		double imageHeight;
 		
+		/* Declare an image object */
 		Image image;
 
+		/* Check if the source is online or local */
 		if (imageName.startsWith("http")) {
 			/*File is a web resource, check that it exists*/
 			if(!mediaExists(imageName)) {
-				/*Add a label to notify user that media was unavailable*/
+				/* Media is not accessible, add a label to notify user */
                 Label label = new Label("Media Unavailable");
                 label.relocate(locationX, locationY);
                 group.getChildren().add(label);
                 
-                /*return to halt creation of image*/
+                /* Return immediately to halt creation of image */
                 return;
 			}
 			
+			/* Instantiate the image */
 			image = new Image("URL:" + imageName);
-			
 		} else {
-			
 			/*File is a local Resource*/
 			image = new Image("file:" + imageName);
-			
 		}
 		
+		/* Get size information */
 		imageWidth = image.getWidth();
 		imageHeight = image.getHeight();
 		
+		/* Create an image view */
 		ImageView imageView = new ImageView(image);
 		imageView = new ImageView();
 		imageView.setImage(image);
 		
+		/* Check if the image is being drawn based on scale */
 		if(scaleBased) {
 		    imageView.setFitWidth(imageWidth * widthSize);
             imageView.setFitHeight(imageHeight *heightSize);
@@ -89,14 +103,24 @@ public class ImageHandler {
 		    imageView.setFitWidth(widthSize);
 		    imageView.setFitHeight(heightSize);
 		}
+		
+		/* Apply rotation and location */
 		imageView.setRotate(rotationDegree);
 		imageView.relocate(locationX, locationY);
 		
+		/* Add the image view to the list */
 		images.add(imageView);
 		
+		/* Add the image to the group */
 		group.getChildren().add(imageView);
 	}
 	
+	/**
+	 * Gets a specific image from the handler.
+	 * 
+	 * @param imageID The ID of the image to get.
+	 * @return The selected image object if it exists.
+	 */
 	public Image getImage(int imageID) {
 	    if(imageID >= 0 && imageID < images.size()) {
 	        return images.get(imageID).getImage();
@@ -105,10 +129,20 @@ public class ImageHandler {
 	    return null;
 	}
 	
+	/**
+	 * Gets the array list of image views.
+	 * 
+	 * @return An array list of all the image views being handled.
+	 */
 	public ArrayList<ImageView> getImages() {
 	    return images;
 	}
 	
+	/**
+	 * Removes a specified image from the group.
+	 * 
+	 * @param imageID The ID of the image to remove.
+	 */
 	public void removeImage(int imageID) {
 	    if(imageID >= 0 && imageID < images.size()) {
 	        group.getChildren().remove(images.get(imageID));
@@ -116,6 +150,9 @@ public class ImageHandler {
 	    }
 	}
 	
+	/**
+	 * Clear all the images.
+	 */
 	public void clearImages() {
 	    images.clear();
 	}
@@ -138,12 +175,16 @@ public class ImageHandler {
           
           /* If a response code of 200 (okay) is received, the media is available */
           return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
-        }
-        catch (Exception e) {
-           return false;
+        } catch (Exception e) {
+            /* Media unavailabe, return false */
+            return false;
         }
     }
     
+    /**
+     * Prints information about the objects being handled to the 
+     * console for debugging purposes.
+     */
     public void debugPrint() {
         System.out.println("### Image Handler ###");
         System.out.println("Handling: " + images.size());
